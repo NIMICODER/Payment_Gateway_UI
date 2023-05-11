a7a4bb3959dc87f04f29c4087c3b713088611c3e
NIMIDEV <99932058+NIMICODER@users.noreply.github.com>
2023-05-11 16:49:40 +0100

    feat: layering work environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LandingPageComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
